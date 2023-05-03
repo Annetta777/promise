0cf5d2eb9aaae7815c2117b1be2e41b1dd5840e8
Annetta777 <loginaanna2000a@gmail.com>
2023-05-03 22:29:42 +0600

    add work with url

--- promise.js
+++ promise.js
@@ -1,14 +1,47 @@
 const USERS_URL = 'http://jsonplaceholder.typicode.com/users'
+const dataContainer = document.querySelector('#data-container')
 
- const result = fetch(USERS_URL, {
-  method: 'GET',
-  headers: {}
-})
-console.log('result', result)
-result
- .then((users) => {
-  console.log('users', users)
- })
- .catch((error) => {
-  console.log('error', error)
- })
+const createUserElement = (text) => {
+  const userElement = document.createElement('li')
+  const userAnchorElement = document.createElement('a')
+  userAnchorElement.href = '#'
+  userAnchorElement.textContent = text
+  userElement.append(userAnchorElement)
+  return userElement
+}
+
+const toggleLoader = () => {
+  const loaderHTML = document.querySelector('#loader')
+  isHidden = loaderHTML.getAttribute('hidden') !== null;
+  if (isHidden) {
+    loaderHTML.removeAttribute('hidden')
+  } else {
+    loaderHTML.setAttribute('hidden', '')
+  }
+}
+
+const getAllUsers = () => {
+  toggleLoader()
+  const result = fetch(USERS_URL, {
+    method: 'GET',
+  })
+  console.log('result', result)
+  result
+  .then((Response) => {
+    return Response.json()
+  })
+   .then((users) => {
+    users.forEach((user) => {
+      const userHTML = createUserElement(user.name)
+      dataContainer.append(userHTML)
+    })
+   })
+   .catch((error) => {
+    console.log('error', error)
+   })  
+   .finally(() => {
+    toggleLoader()
+   })
+}
+
+getAllUsers()
