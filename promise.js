48a15191f3e0b11d6c92018656a2b0a596f0f1bb
Annetta777 <loginaanna2000a@gmail.com>
2023-05-10 13:27:39 +0600

    result

--- promise.js
+++ promise.js
@@ -20,7 +20,9 @@ const toggleLoader = () => {
   }
 }
 
-const getAllUsers = () => {
+//список пользователей
+
+/*const getAllUsers = () => {
   toggleLoader()
   const result = fetch(USERS_URL, {
     method: 'GET',
@@ -42,6 +44,67 @@ const getAllUsers = () => {
    .finally(() => {
     toggleLoader()
    })
+}*/
+
+// список пользователей по ид
+
+const getUsersByIds = (usersId) => {
+  toggleLoader()
+  Promise.all(usersId.map((id) => fetch(`${USERS_URL}/${id}`)))
+  .then((Response) => {
+    return Promise.all(Response.map((element) => element.json()))
+  })
+  .then((users) => {
+    console.log('users', users)
+    users.forEach((user) => {
+      const userHTML = createUserElement(user.name)
+      dataContainer.append(userHTML)
+    })
+  })
+  .catch((error) => {
+    console.log('error', error)
+  })
+  .finally(() => {
+    toggleLoader()
+   })
+}
+getUsersByIds([5, 6, 2, 1])
+
+// работа с фото
+const PHOTOS_URL = 'https://jsonplaceholder.typicode.com/photos'
+
+const createPhotoItem = (url, title) => {
+  const photoItem = document.createElement('li')
+  photoItem.className = 'photo-item'
+
+  const photoImage = document.createElement('img')
+  photoImage.src = url;
+  photoImage.className = 'photo-item__image'
+
+  const photoTitle = document.createElement('h3')
+  photoTitle.innerText = title;
+
+  photoItem.append(photoImage, photoTitle)
+
+  return photoItem
+}
+const getFastestLoadedPhoto = (ids) => {
+  toggleLoader();
+  Promise.race(ids.map((id) => fetch(`${PHOTOS_URL}/${id}`)))
+    .then((Response) => { 
+      return Response.json()
+    })
+    .then((photo) => {
+      const photoHTML = createPhotoItem(photo.url, photo.title)
+      dataContainer.append(photoHTML)
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+    .finally(() => {
+      toggleLoader()
+    })
 }
 
-getAllUsers()
+getFastestLoadedPhoto([60, 12, 55])
+
